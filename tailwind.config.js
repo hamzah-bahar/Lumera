ee8dd38701e47ae19626897ca08bad69087c12a0
Hamzah Bahar <hamzah.bahar@ttawor.com>
2025-06-22 11:28:03 +0300

    add site logo and apply brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: {
+                    200: "#be8e4e85",
+                    400: "#be8e4eab",
+                    500: "#c89e61",
+                },
+                secondary: {
+                    200: "#",
+                    400: "#",
+                    500: "rgb(70 108 140)",
+                },
             },
         },
     },
